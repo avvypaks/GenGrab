f07a3addeb32e7c7699b1f1d70ebd8d2c0c49c43
goirikdg <goirikdg@users.noreply.github.com>
2020-07-27 10:48:06 +0530

    Update gengrab2.py

--- gengrab2.py
+++ gengrab2.py
@@ -1,10 +1,14 @@
 import pandas as pd
 from tkinter.filedialog import askopenfilename,asksaveasfilename
-df=pd.read_csv(askopenfilename(title = "Please select the gene separation file(csv)"))
+df=pd.read_csv(askopenfilename(title = "Please select the csv output"))
 
-#provide the gen name when prompted 
+#enter gene name 
 gene_name = input("Enter Gene Name:" )
+df_check=df[df['Gene']==gene_name]
 
-print(df[df['Gene']==gene_name])
-
-df[df['Gene']==gene_name].to_csv(asksaveasfilename(title = 'Save as Csv',defaultextension='.csv'),index = 0)
+#check if gene is present in the input file
+if (df_check.empty):
+    print("Gene not found!")
+else:
+    print(df[df['Gene']==gene_name])
+    df[df['Gene']==gene_name].to_csv(asksaveasfilename(title = 'Save as Csv',defaultextension='.csv'),index = 0)
